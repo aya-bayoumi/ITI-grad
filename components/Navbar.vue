b7b7f0ef2197a95b0150c23324647962b67458f1
SandyNabil <sandynabil257@gmail.com>
2025-03-26 03:12:17 +0200

    browser name and icon
    favicon and title

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar">
-        navbbar
+        navbar
       <div class="logo"> Qera'a</div>
 
     </nav>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    head: {
+      link: [{ rel: "icon", href: "/favicon.ico" }],
+      title:"Qera'a",
+    },
+  },
+
   devtools: { enabled: true },
     css: ['~/assets/css/main.css'],
    vite: {
